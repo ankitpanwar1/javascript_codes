972f58c11d6d0524ed7121d688576c158ffafd9f
Ankit Panwar <155932035+ankitpanwar1@users.noreply.github.com>
2024-04-01 00:28:13 +0000

    comparision done

---
+++ js_basics/04_comparision.js
@@ -0,0 +1,25 @@
+/* console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2!=1);
+console.log(2==1); */
+
+//comparision data must be same we can not compare a string to a number
+
+/* console.log(null>0); //false
+console.log(null==0);//false
+console.log(null>=0); //true
+// because == and the comparisions operator like < > <= >= works differently
+// comparisions convert null to 0 thats why (3) gives true as 0>=0
+//but (1) give result false as 0>0 is not true
+
+console.log(undefined>0); //false
+console.log(undefined==0);//false
+console.log(undefined>=0);//false */
+
+/* //  ===  strict check 
+console.log("2" == 2); //true as it is converting the data 
+
+console.log("2" === 2); //false as it can not check two data of different
+//types as it is strictly equal operator which check data as well as 
+//datatype */
